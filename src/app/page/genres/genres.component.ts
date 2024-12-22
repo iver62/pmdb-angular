39452d36ef2b3df92c86192b86bca211a7a398f5
iver62 <iver62@hotmail.fr>
2024-12-22 17:46:37 +0100

    Supprimer un genre

--- src/app/page/genres/genres.component.ts
+++ src/app/page/genres/genres.component.ts
@@ -1,19 +1,43 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgPipesModule } from 'ngx-pipes';
+import { catchError, switchMap, tap } from 'rxjs';
 import { GenreService } from '../../services/genre.service';
 
 @Component({
-    selector: 'app-genres',
-    imports: [CommonModule, MatCardModule, NgPipesModule],
-    templateUrl: './genres.component.html',
-    styleUrl: './genres.component.css'
+  selector: 'app-genres',
+  imports: [
+    AsyncPipe,
+    MatCardModule,
+    MatIconModule,
+    MatTooltipModule,
+    NgPipesModule
+  ],
+  templateUrl: './genres.component.html',
+  styleUrl: './genres.component.css'
 })
 export class GenresComponent {
 
+  private _snackBar = inject(MatSnackBar);
+  durationInSeconds = 5;
   genres$ = this.genreService.getAll();
 
   constructor(private genreService: GenreService) { }
+
+  deleteGenre(id: number) {
+    this.genres$ = this.genreService.delete(id).pipe(
+      switchMap(() => this.genreService.getAll()),
+      tap(() => this._snackBar.open('Genre supprimé avec succès', 'Done', { duration: this.durationInSeconds * 1000 })),
+      catchError((error) => {
+        console.error(error);
+        this._snackBar.open('Impossible de supprimer le genre', 'Error', { duration: this.durationInSeconds * 1000 })
+        return this.genreService.getAll();
+      })
+    );
+  }
 
 }
