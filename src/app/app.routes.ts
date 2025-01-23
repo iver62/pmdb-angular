e0fadd4d9187ce7f515a740ebf221ee6d970a60a
iver62 <iver62@hotmail.fr>
2025-01-23 12:43:33 +0100

    Ajout du resolver PersonType dans les routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,198 @@
 import { Routes } from '@angular/router';
 import { ActorsComponent } from './page/actors/actors.component';
 import { AddMovieComponent } from './page/add-movie/add-movie.component';
-import { CostumiersComponent } from './page/costumiers/costumiers.component';
 import { CountriesComponent } from './page/countries/countries.component';
 import { DashboardComponent } from './page/dashboard/dashboard.component';
-import { DecoratorsComponent } from './page/decorators/decorators.component';
-import { DirectorsComponent } from './page/directors/directors.component';
-import { EditorsComponent } from './page/editors/editors.component';
 import { GenresComponent } from './page/genres/genres.component';
-import { MusiciansComponent } from './page/musicians/musicians.component';
-import { PhotographersComponent } from './page/photographers/photographers.component';
-import { ProducersComponent } from './page/producers/producers.component';
-import { ScreenwritersComponent } from './page/screenwriters/screenwriters.component';
+import { personTypeResolver } from './resolvers/person-type.resolver';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'add-movie', component: AddMovieComponent },
-  { path: 'movies', loadComponent: () => import('./page/movies/movies.component').then(c => c.MoviesComponent) },
-  { path: 'movies/:id', loadComponent: () => import('./page/movies/movie-detail/movie-detail.component').then(c => c.MovieDetailComponent) },
-  { path: 'producers', component: ProducersComponent },
-  { path: 'directors', component: DirectorsComponent },
-  { path: 'screenwriters', component: ScreenwritersComponent },
-  { path: 'musicians', component: MusiciansComponent },
-  { path: 'decorators', component: DecoratorsComponent },
-  { path: 'costumiers', component: CostumiersComponent },
-  { path: 'photographers', component: PhotographersComponent },
-  { path: 'editors', component: EditorsComponent },
-  { path: 'actors', component: ActorsComponent },
-  { path: 'genres', component: GenresComponent },
-  { path: 'countries', component: CountriesComponent },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard'
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'add-movie',
+    component: AddMovieComponent
+  },
+  {
+    path: 'movies',
+    loadComponent: () => import('./page/movies/movies.component').then(c => c.MoviesComponent)
+  },
+  {
+    path: 'movies/:id',
+    loadComponent: () => import('./page/movies/movie-detail/movie-detail.component').then(c => c.MovieDetailComponent)
+  },
+  {
+    path: 'producers',
+    loadComponent: () => import('./page/producers/producers.component').then(c => c.ProducersComponent)
+  },
+  {
+    path: 'producers/:id',
+    resolve: {
+      person: personTypeResolver
+    },
+    loadComponent: () => import('./page/person-details/person-details.component').then(c => c.PersonDetailsComponent)
+  },
+  {
+    path: 'directors',
+    loadComponent: () => import('./page/directors/directors.component').then(c => c.DirectorsComponent)
+  },
+  {
+    path: 'directors/:id',
+    resolve: {
+      person: personTypeResolver
+    },
+    loadComponent: () => import('./page/person-details/person-details.component').then(c => c.PersonDetailsComponent)
+  },
+  {
+    path: 'screenwriters',
+    loadComponent: () => import('./page/screenwriters/screenwriters.component').then(c => c.ScreenwritersComponent)
+  },
+  {
+    path: 'screenwriters/:id',
+    resolve: {
+      person: personTypeResolver
+    },
+    loadComponent: () => import('./page/person-details/person-details.component').then(c => c.PersonDetailsComponent)
+  },
+  {
+    path: 'musicians',
+    loadComponent: () => import('./page/musicians/musicians.component').then(c => c.MusiciansComponent)
+  },
+  {
+    path: 'musicians/:id',
+    resolve: {
+      person: personTypeResolver
+    },
+    loadComponent: () => import('./page/person-details/person-details.component').then(c => c.PersonDetailsComponent)
+  },
+  {
+    path: 'decorators',
+    loadComponent: () => import('./page/decorators/decorators.component').then(c => c.DecoratorsComponent)
+  },
+  {
+    path: 'decorators/:id',
+    resolve: {
+      person: personTypeResolver
+    },
+    loadComponent: () => import('./page/person-details/person-details.component').then(c => c.PersonDetailsComponent)
+  },
+  {
+    path: 'costumiers',
+    loadComponent: () => import('./page/costumiers/costumiers.component').then(c => c.CostumiersComponent)
+  },
+  {
+    path: 'costumiers/:id',
+    resolve: {
+      person: personTypeResolver
+    },
+    loadComponent: () => import('./page/person-details/person-details.component').then(c => c.PersonDetailsComponent)
+  },
+  {
+    path: 'photographers',
+    loadComponent: () => import('./page/photographers/photographers.component').then(c => c.PhotographersComponent)
+  },
+  {
+    path: 'photographers/:id',
+    resolve: {
+      person: personTypeResolver
+    },
+    loadComponent: () => import('./page/person-details/person-details.component').then(c => c.PersonDetailsComponent)
+  },
+  {
+    path: 'editors',
+    loadComponent: () => import('./page/editors/editors.component').then(c => c.EditorsComponent)
+  },
+  {
+    path: 'editors/:id',
+    resolve: {
+      person: personTypeResolver
+    },
+    loadComponent: () => import('./page/person-details/person-details.component').then(c => c.PersonDetailsComponent)
+  },
+  {
+    path: 'casters',
+    loadComponent: () => import('./page/casters/casters.component').then(c => c.CastersComponent)
+  },
+  {
+    path: 'casters/:id',
+    resolve: {
+      person: personTypeResolver
+    },
+    loadComponent: () => import('./page/person-details/person-details.component').then(c => c.PersonDetailsComponent)
+  },
+  {
+    path: 'art-directors',
+    loadComponent: () => import('./page/art-directors/art-directors.component').then(c => c.ArtDirectorsComponent)
+  },
+  {
+    path: 'art-directors/:id',
+    resolve: {
+      person: personTypeResolver
+    },
+    loadComponent: () => import('./page/person-details/person-details.component').then(c => c.PersonDetailsComponent)
+  },
+  {
+    path: 'sound-editors',
+    loadComponent: () => import('./page/sound-editors/sound-editors.component').then(c => c.SoundEditorsComponent)
+  },
+  {
+    path: 'sound-editors/:id',
+    resolve: {
+      person: personTypeResolver
+    },
+    loadComponent: () => import('./page/person-details/person-details.component').then(c => c.PersonDetailsComponent)
+  },
+  {
+    path: 'visual-effects-supervisors',
+    loadComponent: () => import('./page/visual-effects-supervisors/visual-effects-supervisors.component').then(c => c.VisualEffectsSupervisorsComponent)
+  },
+  {
+    path: 'visual-effects-supervisors/:id',
+    resolve: {
+      person: personTypeResolver
+    },
+    loadComponent: () => import('./page/person-details/person-details.component').then(c => c.PersonDetailsComponent)
+  },
+  {
+    path: 'makeup-artists',
+    loadComponent: () => import('./page/makeup-artists/makeup-artists.component').then(c => c.MakeupArtistsComponent)
+  },
+  {
+    path: 'makeup-artists/:id',
+    resolve: {
+      person: personTypeResolver
+    },
+    loadComponent: () => import('./page/person-details/person-details.component').then(c => c.PersonDetailsComponent)
+  },
+  {
+    path: 'hair-dressers',
+    loadComponent: () => import('./page/hair-dressers/hair-dressers.component').then(c => c.HairDressersComponent)
+  },
+  {
+    path: 'hair-dressers/:id',
+    resolve: {
+      person: personTypeResolver
+    },
+    loadComponent: () => import('./page/person-details/person-details.component').then(c => c.PersonDetailsComponent)
+  },
+  {
+    path: 'actors',
+    component: ActorsComponent
+  },
+  {
+    path: 'genres',
+    component: GenresComponent
+  },
+  {
+    path: 'countries',
+    component: CountriesComponent
+  },
   // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 ];
--- src/app/resolvers/person-type.resolver.ts
+++ src/app/resolvers/person-type.resolver.ts
@@ -4,25 +4,25 @@ import { catchError, concatMap, map, of, throwError } from 'rxjs';
 import { Person } from '../models';
 import { ArtDirectorService, BaseService, CasterService, CostumierService, DecoratorService, DirectorService, EditorService, HairDresserService, MakeupArtistService, MusicianService, PhotographerService, ProducerService, ScreenwriterService, SoundEditorService, VisualEffectSupervisorsService } from '../services';
 
-// Mapping des types de personnes vers leurs services
-const serviceMap: { [key: string]: BaseService } = {
-  producers: inject(ProducerService),
-  directors: inject(DirectorService),
-  screenwriters: inject(ScreenwriterService),
-  musicians: inject(MusicianService),
-  photographers: inject(PhotographerService),
-  costumiers: inject(CostumierService),
-  decorators: inject(DecoratorService),
-  editors: inject(EditorService),
-  casters: inject(CasterService),
-  'art-directors': inject(ArtDirectorService),
-  'sound-editors': inject(SoundEditorService),
-  'visual-effects-supervisors': inject(VisualEffectSupervisorsService),
-  'makeup-artists': inject(MakeupArtistService),
-  'hair-dressers': inject(HairDresserService)
-};
-
 export const personTypeResolver: ResolveFn<Person> = (route, state) => {
+  // Mapping des types de personnes vers leurs services
+  const serviceMap: { [key: string]: BaseService } = {
+    producers: inject(ProducerService),
+    directors: inject(DirectorService),
+    screenwriters: inject(ScreenwriterService),
+    musicians: inject(MusicianService),
+    photographers: inject(PhotographerService),
+    costumiers: inject(CostumierService),
+    decorators: inject(DecoratorService),
+    editors: inject(EditorService),
+    casters: inject(CasterService),
+    'art-directors': inject(ArtDirectorService),
+    'sound-editors': inject(SoundEditorService),
+    'visual-effects-supervisors': inject(VisualEffectSupervisorsService),
+    'makeup-artists': inject(MakeupArtistService),
+    'hair-dressers': inject(HairDresserService)
+  };
+
   // Récupérer le type depuis l'URL (premier élément du chemin)
   const personType = route.url.at(0)?.path;
 
