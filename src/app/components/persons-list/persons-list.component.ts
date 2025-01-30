2db03a873179fbefebcaba878cf013297b51565f
iver62 <iver62@hotmail.fr>
2025-01-30 23:57:08 +0100

    Composant PersonsList

--- src/app/components/persons-list/persons-list.component.ts
+++ src/app/components/persons-list/persons-list.component.ts
@@ -1,15 +1,33 @@
 import { Component, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgPipesModule } from 'ngx-pipes';
 import { Person } from '../../models';
+import { PersonCardComponent } from './person-card/person-card.component';
 
 @Component({
   selector: 'app-persons-list',
-  imports: [NgPipesModule],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    NgPipesModule,
+    PersonCardComponent
+  ],
   templateUrl: './persons-list.component.html',
   styleUrl: './persons-list.component.css'
 })
 export class PersonsListComponent {
 
+  title = input.required<string>();
   persons = input.required<Person[]>();
+  route = input.required<string>();
+
+  view: 'table' | 'cards' = 'cards';
+
+  switchView(view: 'table' | 'cards') {
+    this.view = view;
+  }
 
 }
