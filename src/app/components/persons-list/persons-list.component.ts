8e77398e3832fc9d0d69760be3c110c878527fc8
iver62 <iver62@hotmail.fr>
2025-02-06 10:16:20 +0100

    Champ de recherche pour les personnes

--- src/app/components/persons-list/persons-list.component.ts
+++ src/app/components/persons-list/persons-list.component.ts
@@ -1,17 +1,23 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, computed, Input, input, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgPipesModule } from 'ngx-pipes';
 import { Person } from '../../models';
-import { PersonCardComponent } from './person-card/person-card.component';
 import { BaseService } from '../../services';
+import { PersonCardComponent } from './person-card/person-card.component';
 
 @Component({
   selector: 'app-persons-list',
   imports: [
+    FormsModule,
     MatButtonModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
     MatTooltipModule,
     NgPipesModule,
     PersonCardComponent
@@ -27,10 +33,39 @@ export class PersonsListComponent {
 
   @Input() service: BaseService;
 
+  filteredPersons = computed(() => {
+    const term = this.searchTermSignal().toLowerCase();
+
+    // Si le terme de recherche est vide, retourner toutes les personnes
+    if (!term) {
+      return this.persons();
+    }
+
+    // Sinon, filtrer les personnes
+    return this.persons().filter(person => person.name.toLowerCase().includes(term));
+  });
+
+  searchTermSignal = signal('');
+
   view: 'table' | 'cards' = 'cards';
 
   switchView(view: 'table' | 'cards') {
     this.view = view;
+  }
+
+  // Getter pour le signal
+  get searchTerm(): string {
+    return this.searchTermSignal();
+  }
+
+  // Setter pour le signal
+  set searchTerm(value: string) {
+    this.searchTermSignal.set(value);
+  }
+
+  // Effacer la recherche
+  clearSearch(): void {
+    this.searchTermSignal.set('');
   }
 
 }
