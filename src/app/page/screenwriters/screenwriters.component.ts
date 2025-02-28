b16310b92ae1e2307a7a5cc791ca145347c72470
iver62 <iver62@hotmail.fr>
2025-02-28 22:22:26 +0100

    Tri et switch images/tableau des scénaristes

--- src/app/page/screenwriters/screenwriters.component.ts
+++ src/app/page/screenwriters/screenwriters.component.ts
@@ -1,12 +1,15 @@
 import { AsyncPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
-import { BehaviorSubject, catchError, map, of, scan, switchMap, tap } from 'rxjs';
-import { InputComponent, PersonsListComponent } from '../../components';
-import { Person, SearchConfig } from '../../models';
+import { BehaviorSubject, catchError, map, Observable, of, scan, switchMap, tap } from 'rxjs';
+import { InputComponent, PersonsListComponent, PersonsTableComponent } from '../../components';
+import { View } from '../../enums';
+import { Person, SearchConfig, SortOption } from '../../models';
 import { ScreenwriterService } from '../../services';
 
 @Component({
@@ -17,29 +20,45 @@ import { ScreenwriterService } from '../../services';
     InputComponent,
     MatButtonModule,
     MatIconModule,
+    MatMenuModule,
+    MatPaginatorModule,
     MatTooltipModule,
-    PersonsListComponent
+    PersonsListComponent,
+    PersonsTableComponent
   ],
   templateUrl: './screenwriters.component.html',
   styleUrl: './screenwriters.component.css'
 })
 export class ScreenwritersComponent {
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  view = View;
   total: number;
+  pageSizeOptions = [25, 50, 100];
+
+  sortOptions: SortOption[] = [
+    { active: 'name', label: 'Nom', direction: 'asc' },
+    { active: 'dateOfBirth', label: 'Date de naissance', direction: '' },
+    { active: 'dateOfDeath', label: 'Date de décès', direction: '' },
+    { active: 'creationDate', label: 'Date de création', direction: '' },
+    { active: 'lastUpdate', label: 'Dernière modification', direction: '' }
+  ]
 
   searchConfig$ = new BehaviorSubject<SearchConfig>(
     {
       page: 0,
       size: 50,
       sort: 'name',
-      direction: 'Ascending',
-      term: ''
+      direction: 'asc',
+      term: '',
+      view: View.CARDS
     }
   );
 
   screenwriters$ = this.searchConfig$.pipe(
     switchMap(config =>
-      this.screenwriterService.get(config.page, config.size, config.term).pipe(
+      this.screenwriterService.get(config.page, config.size, config.term, config.sort, config.direction).pipe(
         tap(response => this.total = +(response.headers.get('X-Total-Count') ?? 0)),
         map(response => (response.body ?? [])),
         catchError(error => {
@@ -48,19 +67,64 @@ export class ScreenwritersComponent {
         })
       )
     ),
-    scan((acc: Person[], result: Person[]) => this.searchConfig$.value.page == 0 ? result : acc.concat(result), []), // Concatène les nouveaux acteurs
+    scan((acc: Person[], result: Person[]) => this.searchConfig$.value.page == 0 || this.searchConfig$.value.view == View.TABLE // Concatène les nouvelles données
+      ? result
+      : acc.concat(result), []
+    )
+  );
+
+  sorts$: Observable<SortOption[]> = this.searchConfig$.pipe(
+    map(config =>
+      this.sortOptions.map(option => (
+        {
+          ...option,
+          direction: option.active === config.sort ? config.direction : '' // Met à jour la direction du tri
+        })
+      )
+    )
   );
 
   constructor(public screenwriterService: ScreenwriterService) { }
 
-  onSearch(event: string) {
+  switchView(view: View) {
     this.searchConfig$.next(
       {
         ...this.searchConfig$.value,
         page: 0,
-        term: event
+        view: view
       }
     );
+  }
+
+  onSelectSort(selectedSort: SortOption) {
+    this.onSort({ active: selectedSort.active, direction: selectedSort.direction === 'asc' ? 'desc' : 'asc' }); // Déclenche l'événement de tri
+  }
+
+  onSort(event: { active: string, direction: 'asc' | 'desc' }) {
+    if (this.searchConfig$.value.view == View.TABLE) {
+      this.paginator.firstPage();
+    }
+
+    this.searchConfig$.next(
+      {
+        ...this.searchConfig$.value,
+        page: 0,
+        sort: event.active,
+        direction: event.direction
+      }
+    );
+  }
+
+  onSearch(event: string) {
+    if (typeof event === 'string') {
+      this.searchConfig$.next(
+        {
+          ...this.searchConfig$.value,
+          page: 0,
+          term: event
+        }
+      );
+    }
   }
 
   onScroll() {
@@ -72,4 +136,13 @@ export class ScreenwritersComponent {
     );
   }
 
+  onPageChange(event: PageEvent) {
+    this.searchConfig$.next(
+      {
+        ...this.searchConfig$.value,
+        page: event.pageIndex,
+        size: event.pageSize
+      }
+    );
+  }
 }
