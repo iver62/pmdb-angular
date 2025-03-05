14f4caeeb7db9ade35412a2f4c0b84ebf871b37e
iver62 <iver62@hotmail.fr>
2025-03-05 12:25:17 +0100

    New toolbar

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,28 +1,34 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, computed, EventEmitter, input, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { View } from '../../enums';
+import { Direction, View } from '../../enums';
 import { SortOption } from '../../models';
 import { FiltersDialogComponent } from '../filters-dialog/filters-dialog.component';
+import { NgPipesModule } from 'ngx-pipes';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-toolbar',
   imports: [
+    JsonPipe,
     MatButtonModule,
     MatDialogModule,
     MatIconModule,
     MatMenuModule,
-    MatTooltipModule
+    MatTooltipModule,
+    NgPipesModule
   ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
 export class ToolbarComponent {
 
-  @Input() sorts: SortOption[];
+  sorts = input.required<SortOption[]>();
+
+  selectedSort = computed(() => this.sorts().find(s => ['asc', 'desc'].includes(s.direction)));
 
   @Output() filter = new EventEmitter();
   @Output() switchView = new EventEmitter<View>();
@@ -31,7 +37,10 @@ export class ToolbarComponent {
   view = View;
   currentView = View.CARDS;
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog) {
+    console.log(this.sorts);
+
+  }
 
   openFiltersDialog() {
     const dialogRef = this.dialog.open(FiltersDialogComponent, {
