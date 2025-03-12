40324f454a98298dffe0f72160340983af600797
iver62 <iver62@hotmail.fr>
2025-03-12 17:10:37 +0100

    Bouton pour supprimer les dates du date range picker

--- src/app/components/date-range-picker/date-range-picker.component.ts
+++ src/app/components/date-range-picker/date-range-picker.component.ts
@@ -1,15 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-date-range-picker',
   imports: [
+    MatButtonModule,
     MatDatepickerModule,
     MatFormFieldModule,
+    MatIconModule,
     MatInputModule,
+    MatTooltipModule,
     ReactiveFormsModule
   ],
   templateUrl: './date-range-picker.component.html',
@@ -18,8 +24,14 @@ import { MatInputModule } from '@angular/material/input';
 export class DateRangePickerComponent {
 
   @Input() label: string;
-  @Input() startDatePlaceholder: string;
-  @Input() endDatePlaceholder: string;
-  @Input() startDateControl: FormControl;
-  @Input() endDateControl: FormControl;
+  @Input() fromDatePlaceholder: string;
+  @Input() toDatePlaceholder: string;
+  @Input() fromDateControl: FormControl;
+  @Input() toDateControl: FormControl;
+
+  clearDates() {
+    this.fromDateControl.reset();
+    this.toDateControl.reset();
+  }
+
 }
