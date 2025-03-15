f4bf0f83b9a9bff9109d020928da0a23ddaac876
iver62 <iver62@hotmail.fr>
2025-03-15 11:41:50 +0100

    Configuration de keycloak

---
+++ src/keycloak-init.ts
@@ -0,0 +1,32 @@
+import { AutoRefreshTokenService, createInterceptorCondition, INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG, IncludeBearerTokenCondition, provideKeycloak, UserActivityService, withAutoRefreshToken } from "keycloak-angular";
+
+const localhostCondition = createInterceptorCondition<IncludeBearerTokenCondition>({
+  urlPattern: /^(http:\/\/localhost:8080)(\/.*)?$/i
+});
+
+export const provideKeycloakAngular = () =>
+  provideKeycloak({
+    config: {
+      url: 'http://localhost:8080',
+      realm: 'pmdb',
+      clientId: 'angular-app'
+    },
+    initOptions: {
+      onLoad: 'login-required',
+      silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
+    },
+    features: [
+      withAutoRefreshToken({
+        onInactivityTimeout: 'logout',
+        sessionTimeout: 60000
+      })
+    ],
+    providers: [
+      AutoRefreshTokenService,
+      UserActivityService,
+      {
+        provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+        useValue: [localhostCondition]
+      }
+    ]
+  });
