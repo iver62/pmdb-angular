5c3d1b5fe12cc34158afc7b55c03043e252c571e
iver62 <iver62@hotmail.fr>
2025-03-24 22:56:55 +0100

    Etat de la checkbox dans le multiselect

--- src/app/components/multiselect/multiselect.component.ts
+++ src/app/components/multiselect/multiselect.component.ts
@@ -1,14 +1,14 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, computed, effect, EventEmitter, input, Output, signal } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSelectModule } from '@angular/material/select';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 import { EMPTY_STRING } from '../../app.component';
 import { DelayedInputDirective } from '../../directives';
-import { Country, Genre } from '../../models';
+import { Country, Genre, User } from '../../models';
 
 @Component({
   selector: 'app-multiselect',
@@ -27,14 +27,28 @@ import { Country, Genre } from '../../models';
 })
 export class MultiselectComponent {
 
-  @Input() label: string;
-  @Input() placeholder: string;
-  @Input() placeholderLabel: string;
-  @Input() noEntriesFoundLabel: string;
-  @Input() items: Country[] | Genre[];
-  @Input() control: FormControl;
+  label = input.required<string>();
+  placeholder = input.required<string>();
+  placeholderLabel = input.required<string>();
+  noEntriesFoundLabel = input.required<string>();
+  items = input.required<Country[] | Genre[] | User[]>();
+  control = input.required<FormControl>();
 
   @Output() update = new EventEmitter<string>();
+
+  // Signal pour stocker les valeurs du formulaire
+  selectedValues = signal<(Country | Genre | User)[]>([]);
+
+  checkboxChecked = computed(() => this.selectedValues()?.length === this.items()?.length);
+  checkboxIndeterminate = computed(() => !this.checkboxChecked() && this.selectedValues()?.length > 0);
+
+  constructor() {
+    effect(() => this.selectedValues.set(this.control().value || []));
+  }
+
+  selectionChange(event: MatSelectChange) {
+    this.selectedValues.set(event.value);
+  }
 
   openedChange(event: boolean) {
     if (event) {
@@ -47,13 +61,15 @@ export class MultiselectComponent {
   }
 
   onToggleAll(event: boolean) {
-    this.control.patchValue(event ? this.items : []);
-    this.control.markAsDirty();
+    this.selectedValues.set(event ? this.items() : [])
+    this.control().patchValue(event ? this.items() : []);
+    this.control().markAsDirty();
   }
 
   clearSelection() {
-    this.control.patchValue([]);
-    this.control.markAsDirty();
+    this.selectedValues.set([])
+    this.control().patchValue([]);
+    this.control().markAsDirty();
   }
 
   compareObjects(o1: Country | Genre, o2: Country | Genre) {
